d8b138cfbb48f7fb36028a3c878808ec88d5cba8
Ziya <ziyagunay06@gmail.com>
2021-12-27 22:15:16 +0100

    day8

---
+++ src/test/java/com/cybertek/tests/day8_types_of_elements_2/ListOfElements.java
@@ -0,0 +1,38 @@
+package com.cybertek.tests.day8_types_of_elements_2;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ListOfElements {
+  WebDriver driver;
+   @BeforeMethod
+   public void setup(){
+
+       driver= WebDriverFactory.getDriver("edge");
+
+   }
+   @AfterMethod
+   public void tearDown() throws InterruptedException {
+       Thread.sleep(2000);
+       driver.quit();
+   }
+
+
+
+    @Test
+    public void test1(){
+    driver.get("http://practice.cybertekschool.com/multiple_buttons");
+
+
+    }
+
+    @Test
+    public void test2(){
+
+
+    }
+}
