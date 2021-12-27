d4631015914baebd9dabe613bf8085b14cb8bb67
Ziya <ziyagunay06@gmail.com>
2021-12-27 22:48:33 +0100

    List of Webelements added

--- src/test/java/com/cybertek/tests/day8_types_of_elements_2/ListOfElements.java
+++ src/test/java/com/cybertek/tests/day8_types_of_elements_2/ListOfElements.java
@@ -1,11 +1,15 @@
 package com.cybertek.tests.day8_types_of_elements_2;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class ListOfElements {
   WebDriver driver;
@@ -27,12 +31,29 @@ public class ListOfElements {
     public void test1(){
     driver.get("http://practice.cybertekschool.com/multiple_buttons");
 
+        List<WebElement> buttons = driver.findElements(By.tagName("button"));
 
-    }
+        System.out.println("buttons.size() = " + buttons.size());
+        Assert.assertEquals(6,6,"verify buttons size");
+// iter + enter  to get each loop with shortcut
+        for (WebElement button : buttons) {
+            System.out.println("button.getText() = " + button.getText());
+            System.out.println("button.isDisplayed() = " + button.isDisplayed());
+            Assert.assertTrue(button.isDisplayed(),"verify buttons are displayed");
+        }
+
+        // click second button
+
+        buttons.get(1).click();
+
+        }
+
+
 
     @Test
     public void test2(){
-
+        driver.get("http://practice.cybertekschool.com/multiple_buttons");
+        List<WebElement> buttons = driver.findElements(By.tagName("button"));
 
     }
 }
